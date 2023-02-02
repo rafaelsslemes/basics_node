b4060bb3e81914b483c93099257093a6564b4327
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-02 13:02:28 -0300

    Basic math operations

---
+++ module_imports/basic_operations.js
@@ -0,0 +1,16 @@
+const value1 = 20;
+const value2 = 10;
+
+console.log('Informed values: ' + value1 + ', ' + value2);
+
+var multiplication = value1 * value2;
+console.log('Multiplication: ' + multiplication);
+
+var division = value1 / value2;
+console.log('Division: ' + division);
+
+var sum = value1 + value2;
+console.log('Sum: ' + sum);
+
+var difference = value1 - value2;
+console.log('Difference: ' + difference);
