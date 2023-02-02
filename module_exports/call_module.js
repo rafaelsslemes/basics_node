4c253ad08261c84a3344fc16bca87888a56af9c7
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-02 16:41:00 -0300

    increment

--- module_exports/call_module.js
+++ module_exports/call_module.js
@@ -1,5 +1,9 @@
 const my_module = require('./reusable_functions');  // Always use relative path
 const imported_sum = my_module.sum;
 
+// Option 1
+my_module.sum(50,50);
+
+// Option 2
 imported_sum(10, 20);
 imported_sum(30, 40);
