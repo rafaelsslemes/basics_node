a7f8d035c4c0ea6bfceee27d7902a2d41185e6b5
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-08 13:09:44 -0300

    Path .join e absolute

--- core_modules/path/index.js
+++ core_modules/path/index.js
@@ -5,3 +5,12 @@ const filepath = '/files/photos/2023/john.png';
 console.log(path.dirname(filepath));
 console.log(path.basename(filepath));
 console.log(path.extname(filepath));
+
+console.log(path.resolve('file.txt'));
+
+const username = 'Peter';
+const rootDir = 'Users';
+const filename = 'Profile.pdf';
+
+const formatedPath = path.join('/', rootDir, username,filename);
+console.log(formatedPath);
