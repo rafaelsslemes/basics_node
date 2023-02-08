a001fe7cb33c6f69629da7d61d18ffbe02221d02
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-08 13:02:49 -0300

    Get path information

---
+++ core_modules/path/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const filepath = '/files/photos/2023/john.png';
+
+console.log(path.dirname(filepath));
+console.log(path.basename(filepath));
+console.log(path.extname(filepath));
